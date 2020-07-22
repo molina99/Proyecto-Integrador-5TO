f31f0562b19a2f6be3bf7b10b26b93ec7562b11a
molina99 <darkmolina99@gmail.com>
2020-07-22 07:11:04 -0500

    Base form

---
+++ back-end/setup/app.js
@@ -0,0 +1,65 @@
+;
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const cors = require('cors')
+require('../config/database')
+
+let app = express()
+
+/**
+ * Import Routes
+ */
+//..............
+
+let session = require('express-session')
+let sess = {
+    secret: process.env.KEY_SESSION,
+    resave: false,
+    saveUninitialized: true,
+    name: 'sessionID',
+    cookie: {
+        httpOnly: false,
+        maxAge: parseInt(process.env.TIME)
+    }
+}
+
+/**
+ * For access client
+ *
+ * @type {{origin: string, optionsSuccessStatus: number}}
+ */
+let corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200
+}
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+/**
+ * CORS
+ */
+app.use(cors(corsOptions))
+
+/**
+ * SESSION
+ */
+app.use(session(sess))
+
+/**
+ * PASSPORT
+ */
+app.use(passport.initialize())
+app.use(passport.session())
+
+/**
+ * Export Routes
+ */
+//..............
+
+module.exports = app
